1151c9e1f5f9c108380977cccd252d67295bfb9a
ypjun100 <ypjun100@gmail.com>
2024-06-06 12:35:55 +0000

    FIX: `column_cleaner` 절차 수정 * 앞 단계에서 열이 삭제된 경우 그 뒤의 절차를 생략하도록 변경

--- src/autobahn/preprocessing/column_cleaner.py
+++ src/autobahn/preprocessing/column_cleaner.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 # Remove columns which don't have any information
 def run(dataframe: pd.DataFrame):
+    # Delete columns which don't have name
+    for column in dataframe.columns[dataframe.columns.str.contains('unnamed',case = False)]:
+        dataframe.drop(column, axis=1, inplace=True)
+
     for column in dataframe.columns:
         # If column have only na values, delete column
         if sum(dataframe[column].isna()) == len(dataframe):
@@ -11,11 +15,8 @@ def run(dataframe: pd.DataFrame):
         # Delete columns which have one unique value
         if len(dataframe[column].dropna(axis=0).unique()) == 1:
             dataframe.drop(column, axis=1, inplace=True)
+            continue
 
         # If ratio of missing values in column is over 90%, delete column
         if int(len(dataframe) * 0.9) <= sum(dataframe[column].isna()):
             dataframe.drop(column, axis=1, inplace=True)
-    
-    # Delete columns which don't have name
-    for column in dataframe.columns[dataframe.columns.str.contains('unnamed',case = False)]:
-        dataframe.drop(column, axis=1, inplace=True)
