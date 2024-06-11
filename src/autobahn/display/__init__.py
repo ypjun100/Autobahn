6b7bbd5ec359fcba91d5d227efca5a36bda4a10d
ypjun100 <ypjun100@gmail.com>
2024-06-11 05:25:27 +0000

    FEAT: 데이터 모델링 및 모델 저장 구현

--- src/autobahn/display/__init__.py
+++ src/autobahn/display/__init__.py
@@ -1,13 +1,15 @@
 import io
 import codecs
+from datetime import datetime
 import functools
 import pandas as pd
 import ipywidgets as widgets
 
+import autobahn.modeling as modeling
 from autobahn.preprocessing import scaler
 import autobahn.preprocessing as preprocessing
 from autobahn.utils.dataset import get_dataset_statistics
-from autobahn.preprocessing import type_converter, missing_value
+from autobahn.preprocessing import type_converter, missing_value, category_encoder
 
 class Tabs:
     NUMBER_OF_TABS = 5
@@ -15,10 +17,12 @@ class Tabs:
 
     def __init__(self):
         self.tab = widgets.Tab()
+        self.filename = ''                # 업로드한 파일 이름
         self.dataframe = None             # 데이터 변수
         self.dependent_col = ""           # 종속변수 명
         self.is_echo = False              # Dropdown 에코 방지
         self.scaling_method = {}          # 각 변수별 스케일링 방식 지정 (기본값: False)
+        self.result_model = None          # 최종 도출 모델
 
         # 데이터프레임 출력이 생략되는 현상 방지
         pd.set_option('display.max_colwidth', None)
@@ -44,6 +48,7 @@ class Tabs:
     # 0 - Upload File
     ##################
     def upload_file(self, change):
+        self.filename = change['new'][0]['name']
         content = change['new'][0]['content']
         self.dataframe = pd.read_csv(io.StringIO(codecs.decode(content)), na_values=missing_value.MISSING_VALUE_SYMBOL)
         preprocessing.pre_all_in_one(self.dataframe) # 데이터 전처리
@@ -112,16 +117,22 @@ class Tabs:
         missing_value.run(self.dataframe, self.dependent_col)
         self.data_preprocessing_loading.value += 5
 
+        # Dataset encoding
+        category_encoder.run(self.dataframe)
+        self.data_preprocessing_loading.value += 3
+
         # Dataset scaling
         for column in self.scaling_method.keys():
             if self.scaling_method[column] == 'Normalize':
                 scaler.normalize(self.dataframe, [column])
             elif self.scaling_method[column] == 'Standardize':
                 scaler.standardize(self.dataframe, [column])
-        self.data_preprocessing_loading.value += 3
+        self.data_preprocessing_loading.value += 2
 
         self.update_final_dataset_view()
         self.tab.selected_index = 3
+        self.data_preprocessing_loading.value = 0
+        self.data_preprocessing_loading.description = ""
         
     def update_data_preprocessing_view(self):
         children = []
@@ -155,7 +166,9 @@ class Tabs:
         return self.final_dataset_vbox
     
     def on_start_modeling(self, _):
+        self.update_auto_modeling_view()
         self.tab.selected_index = 4
+        self.start_modeling()
     
     def update_final_dataset_view(self):
         children = []
@@ -166,7 +179,7 @@ class Tabs:
         children.append(widgets.HTML(value="<h4>최종 데이터셋 통계치</h4>"))
         children.append(widgets.HTML(value=get_dataset_statistics(self.dataframe).style.hide()._repr_html_()))
         children.append(widgets.HTML(value="<hr/>"))
-        start_modeling_button = widgets.Button(description="Start Auto Modeling")
+        start_modeling_button = widgets.Button(description="Start Modeling")
         start_modeling_button.on_click(self.on_start_modeling)
         children.append(start_modeling_button)
         self.final_dataset_vbox.children = children
@@ -176,10 +189,33 @@ class Tabs:
     # 4 - Auto Modeling
     ##################
     def get_auto_modeling_view(self):
-        return widgets.VBox([])
+        self.auto_modeling_vbox = widgets.VBox([])
+        return self.auto_modeling_vbox
     
     def update_auto_modeling_view(self):
-        pass
+        children = []
+        self.auto_modeling_output = widgets.Output()
+        children.append(self.auto_modeling_output)
+        self.auto_modeling_result_table = widgets.HTML()
+        children.append(self.auto_modeling_result_table)
+        children.append(widgets.HTML(value="<hr/>"))
+        save_model_button = widgets.Button(description="Save Model")
+        save_model_button.on_click(self.on_save_model)
+        children.append(save_model_button)
+        self.auto_modeling_vbox.children = children
+
+    def start_modeling(self):
+        self.auto_modeling_output.clear_output()
+        with self.auto_modeling_output:
+            result = modeling.classification(self.dataframe, self.dependent_col)
+            self.model = result['model']
+            self.auto_modeling_result_table.value = result['result_table']._repr_html_()
+    
+    def on_save_model(self, _):
+        filename = self.filename + "-" + datetime.today().strftime("%Y%m%d%H%M%S")
+        if self.model != None:
+            modeling.save(self.model, filename)
+            print('모델 저장 완료 -', filename)
 
 
 def display():
---
+++ src/autobahn/modeling/__init__.py
@@ -0,0 +1,14 @@
+import gc
+import pandas as pd
+from pycaret.classification import *
+
+def classification(dataset:pd.DataFrame, target: str) -> dict:
+    setup(data=dataset, target=target, train_size=0.8, session_id=42)
+    result = pull()
+    print(result)
+    best_model = compare_models(sort='Accuracy', n_select=1, fold=2)
+    result = pull()
+    return {'model': best_model, 'result_table': result}
+
+def save(model, filename: str):
+    save_model(model, filename, verbose=False)
