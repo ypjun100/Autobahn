63c6f6778355a2d75c626e1c6aa1f326a4b4db7b
ypjun100 <ypjun100@gmail.com>
2024-06-12 08:14:29 +0000

    FEAT: scaler 계산 최적화

--- src/autobahn/preprocessing/scaler.py
+++ src/autobahn/preprocessing/scaler.py
@@ -7,7 +7,8 @@ class Normalizer:
         self.x_min = 0
         self.x_max = 0
     
-    def fit_transform(self, arr: list):
+    def fit_transform(self, series: pd.Series):
+        arr = series.to_numpy()
         self.x_min = min(arr)
         self.x_max = max(arr)
         return (arr - self.x_min) / (self.x_max - self.x_min)
@@ -21,11 +22,12 @@ class Standardizer:
         self.mu = 0
         self.sigma = 0
     
-    def fit_transform(self, arr: np.array):
-        _arr = arr[~np.isnan(arr)]
-        self.mu = np.mean(_arr)
-        self.sigma = np.std(_arr)
-        return (arr - self.mu) / self.sigma
+    def fit_transform(self, series: pd.Series):
+        _series = series.dropna()
+        arr = _series.to_numpy()
+        self.mu = np.mean(arr)
+        self.sigma = np.std(arr)
+        return (series.to_numpy() - self.mu) / self.sigma
     
     def transform(self, x):
         return (x - self.mu) / self.sigma
@@ -34,7 +36,7 @@ class Standardizer:
 def normalize(dataframe: pd.DataFrame, columns: list[str], pipeline = None):
     for column in columns:
         normalizer = Normalizer()
-        dataframe[column] = normalizer.fit_transform(dataframe[column].to_numpy()).reshape(-1, 1)
+        dataframe[column] = normalizer.fit_transform(dataframe[column]).reshape(-1, 1)
 
         if pipeline != None:
             pipeline.set_scaling(column, 'Normalize', normalizer)
@@ -44,7 +46,7 @@ def normalize(dataframe: pd.DataFrame, columns: list[str], pipeline = None):
 def standardize(dataframe: pd.DataFrame, columns: list[str], pipeline = None):
     for column in columns:
         standardizer = Standardizer()
-        dataframe[column] = standardizer.fit_transform(dataframe[column].to_numpy())
+        dataframe[column] = standardizer.fit_transform(dataframe[column])
 
         if pipeline != None:
             pipeline.set_scaling(column, 'Standardize', standardizer)
