db89d051677421de8561f9128b986dd57aaf1ff7
ypjun100 <ypjun100@gmail.com>
2024-06-12 03:03:57 +0000

    FEAT: pipeline 구조 변경

--- src/autobahn/utils/__init__.py
+++ src/autobahn/utils/__init__.py
@@ -3,32 +3,31 @@ import pandas as pd
 
 class Pipeline:
     def __init__(self, dataframe: pd.DataFrame, target: str):
-        self.data = {'target': '', 'columns': {}}
+        self.data = {}
         for column in dataframe.columns:
             if column == target:
-                self.data['target'] = target
                 continue
-            self.data['columns'][column] = {"type": "", "scaling": "False", "normalizer": None, "standardizer": None, "encoding": "False", "encoder": None, "category": []}
+            self.data[column] = {"type": "", "scaling": "False", "normalizer": None, "standardizer": None, "encoding": "False", "encoder": None, "category": []}
             if dataframe[column].dtypes in ['bool', 'category']:
-                self.data['columns'][column]['type'] = 'category'
-                self.data['columns'][column]['category'] = self.get_category(dataframe[column])
+                self.data[column]['type'] = 'category'
+                self.data[column]['category'] = self.get_category(dataframe[column])
             else:
-                self.data['columns'][column]['type'] = 'numeric'
+                self.data[column]['type'] = 'numeric'
     
     def get_category(self, series: pd.Series):
         return list(map(str, series.unique()))
     
     def set_scaling(self, column: str, scaling_method: str, scaler: object):
         if scaling_method == 'Normalize':
-            self.data['columns'][column]['scaling'] = scaling_method
-            self.data['columns'][column]['normalizer'] = scaler
+            self.data[column]['scaling'] = scaling_method
+            self.data[column]['normalizer'] = scaler
         elif scaling_method == 'Standardize':
-            self.data['columns'][column]['scaling'] = scaling_method
-            self.data['columns'][column]['normalizer'] = scaler
+            self.data[column]['scaling'] = scaling_method
+            self.data[column]['normalizer'] = scaler
 
     def set_encoding(self, column: str, encoder: object):
-        self.data['columns'][column]['encoding'] = "True"
-        self.data['columns'][column]['encoder'] = encoder
+        self.data[column]['encoding'] = "True"
+        self.data[column]['encoder'] = encoder
     
     def get_pipeline(self):
         return self.data
